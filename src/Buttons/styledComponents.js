cdaa78acbfe0d48372c271fb4458de64d65bfc42
NithinKore56 <kkorenithin@gmail.com>
2024-05-01 03:22:59 +0000

    assignment6

---
+++ src/Buttons/styledComponents.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const ButtonLiContainer = styled.li`
+    display: flex;
+    list-style: none; 
+    flex-direction: row;
+    justify-content: center; 
+    width: 50%; 
+`
+
+export const ButtonImage = styled.button` 
+    background-color: transparent;
+    cursor: pointer;
+    border: 0;
+`
+
+export const ImageItem = styled.img`
+    width: 150px;
+    height: 150px;
+`
